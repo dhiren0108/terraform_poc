74a48ebc7de81258eed77424dc8e6ee15c22fe3d
dhiren0108 <dhiren.singh0108@gmail.com>
2026-02-19 08:17:55 +0530

    Add Terraform variables for configuration

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "prefix" {
+  default = "tfstate-mlsa"
+}
+
+variable "location" {
+  default = "Central India"
+}
+
+variable "admin_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "subscription_id" {
+  type      = string
+  sensitive = true
+}
